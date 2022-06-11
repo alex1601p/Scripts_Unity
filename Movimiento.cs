0233ecb97ac97accbd52d67831d7350148e82944
alex1601 <ap16012001@gmail.com>
2022-06-10 23:32:47 -0600

    Script de mov 2d mejorado

--- Movimiento.cs
+++ Movimiento.cs
@@ -1,45 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Movimiento : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    public float Speed = 1.0f;
-    public float RotationSpeed = 1.0f;
-    public float JumpForce = 1.0f;
+    // Velocidad a la cual el personaje se movera
+    public float speed = 3f;
 
-    private Rigidbody Physics;
+    private Rigidbody2D playerRb;
+    private Vector2 moveInput;
 
-    // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-
-        Physics = GetComponent<Rigidbody>();
+        playerRb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+    // Llama los inputs cada frame
     void Update()
+    {   
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        moveInput = new Vector2(moveX, moveY);
+    }
+
+    // Genera el movimiento del personaje
+    private void FixedUpdate()
     {
-        //Movimiento
-        float horizontal = Input.GetAxis("Horizontal");
-
-        float vertical = Input.GetAxis("Vertical");
-
-        transform.Translate(new Vector3(horizontal, 0.0f, vertical) * Time.deltaTime * Speed);
-
-        //Rotacion camara
-        float rotationX = Input.GetAxis("Mouse X");
-        float rotationY = Input.GetAxis("Mouse Y");
-        Debug.Log(rotationY);
-        // (Y,X)
-        transform.Rotate(new Vector3(-rotationY * Time.deltaTime * RotationSpeed, rotationX * Time.deltaTime * RotationSpeed, 0));
-
-        //Salto
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Physics.AddForce(new Vector3(0, JumpForce, 0),ForceMode.Impulse);
-        }
+        playerRb.MovePosition(playerRb.position + moveInput * speed * Time.fixedDeltaTime);
     }
 }
